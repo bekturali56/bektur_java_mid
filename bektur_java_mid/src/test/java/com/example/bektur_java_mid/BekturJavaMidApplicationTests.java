13d1a10a7ca2981379f14b615880213b24109d60
Bekturali Orozov <bekturaliorozov@gmail.com>
2024-03-25 13:26:15 +0600

    Initial commit

---
+++ bektur_java_mid/src/test/java/com/example/bektur_java_mid/BekturJavaMidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bektur_java_mid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BekturJavaMidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
